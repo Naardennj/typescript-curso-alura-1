74f6722436c93c242e3266b453d897c790975056
Naardennj <gabriel.angelis787@gmail.com>
2024-12-05 10:46:23 -0300

    Melhorando o codigo Criando uma programação defensiva para "date" Finalizado primeira parte do curso de typescript

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,13 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negocicoes: Array<Negociacao> = [];
+    private negocicoes: Negociacao[] = [];
 
     adiciona(neogicicao: Negociacao) {
         this.negocicoes.push(neogicicao);
     };
 
-    lista(): ReadonlyArray<Negociacao> {
+    lista(): readonly Negociacao[] {
         return this.negocicoes;
     };
 };
