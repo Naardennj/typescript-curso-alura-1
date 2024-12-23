875f667b0deb26130752ef3b936334cab1a79ec9
Naardennj <gabriel.angelis787@gmail.com>
2024-12-23 12:27:55 -0300

    Adicionando ao codigo a utilização de API

--- app/dist/js/app.js
+++ app/dist/js/app.js
@@ -10,3 +10,12 @@ if (form) {
 else {
     throw Error('Não foi possivel inicializar a aaplicação');
 }
+const botaoImporta = document.querySelector('#botao-importa');
+if (botaoImporta) {
+    botaoImporta.addEventListener('click', () => {
+        controller.importaDados();
+    });
+}
+else {
+    throw Error('Botão importa não foi encontradado');
+}
